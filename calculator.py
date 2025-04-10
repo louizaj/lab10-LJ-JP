340076453bc082b79beea0637d0f5daef9f7221c
louizaj <118773547+louizaj@users.noreply.github.com>
2025-04-10 14:28:40 -0400

    merged changes

--- calculator.py
+++ calculator.py
@@ -11,13 +11,8 @@ One function per operation, in order.
 
 import math
 
-# First example
 def add(a, b):
     return a + b
-
-def sub(a, b):
-    return a - b
-import math
 
 def square_root(a):
     try:
@@ -30,17 +25,10 @@ def square_root(a):
         print(e)
 
 def hypotenuse(a,b):
-
     return math.hypot(a,b)
-
-def add(a, b):
-    return a + b
 
 def subtract(a, b):
     return a - b
-
-def multiply(a, b):
-    return a * b
 
 def logarithm(a, b):
     if a <= 1:
@@ -51,9 +39,6 @@ def logarithm(a, b):
 
     return math.log(b, a)  # log base a of b
 
-def exponent(a, b):
-    return math.pow(a, b)
-
 def mul(a, b):
     return a * b
 
@@ -61,13 +46,6 @@ def div(a, b):
     if a == 0:
         raise ZeroDivisionError
     return b / a
-
-def log(a, b):
-    if b <= 0:
-        raise ValueError("Cannot perform logarithm on negative numbers")
-    if a < 1:
-        raise ValueError("Logarithm is not defined for base of 1 or less")
-    return math.log(b, a)
 
 def exp(a, b):
     return a ** b
