fb4d4337b6024bfbec1767f655eb2ce42305d5e9
louizaj <118773547+louizaj@users.noreply.github.com>
2025-04-10 14:49:54 -0400

    merged changes

--- calculator.py
+++ calculator.py
@@ -15,14 +15,14 @@ def add(a, b):
     return a + b
 
 def square_root(a):
-    try:
-        if a < 0:
-            raise ValueError("Cannot get root of a negative number")
+    # try:
+    if a < 0:
+        raise ValueError
 
-        return math.sqrt(a)
+    return math.sqrt(a)
 
-    except ValueError as e:
-        print(e)
+    # except ValueError as e:
+    #     print(e)
 
 def hypotenuse(a,b):
     return math.hypot(a,b)
@@ -32,10 +32,10 @@ def subtract(a, b):
 
 def logarithm(a, b):
     if a <= 1:
-        raise ValueError("Base cannot be <= 1")
+        raise ValueError
 
     if b <= 0:
-        raise ValueError("Argument cannot be <= 0")
+        raise ValueError
 
     return math.log(b, a)  # log base a of b
 
